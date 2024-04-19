d61dfcd81a71d1891d8a468ec47a25a93f159b78
angelika <161327272+Tamayo-Angelika-BSCS32E1@users.noreply.github.com>
2024-04-19 22:11:05 +0800

    update domain

--- TodoList.Domain/ToDo.cs
+++ TodoList.Domain/ToDo.cs
@@ -4,11 +4,20 @@ namespace TodoList.Domain;
 
 public class ToDo
 {
+    public bool Overdue
+    {
+        get
+        {
+            return DueDate.HasValue && DueDate.Value < DateTime.Today;
+        }
+    }
+
     public int Id { get; set; }
     public string Description { get; set; } = string.Empty;
     public DateTime? DueDate { get; set; }
-    public Category Category { get; set; } = Category.Adventure;
+    public Category Category { get; set; } = Category.Adventure; // Correct enum usage
     public bool IsActive { get; set; } = true;
-    public Status Status { get; set; } = Status.New;
+    public Status Status { get; set; } = Status.New; // Correct enum usage
+    public string StatusId { get; set; }
 
 }
