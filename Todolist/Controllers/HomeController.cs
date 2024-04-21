c0686c981887e458cb7bc394db6ddd8e7f1ede4f
angelika <161327272+Tamayo-Angelika-BSCS32E1@users.noreply.github.com>
2024-04-21 17:29:56 +0800

    updated homecontroller

--- Todolist/Controllers/HomeController.cs
+++ Todolist/Controllers/HomeController.cs
@@ -15,54 +15,16 @@ namespace Todolist.Controllers
     {
         private readonly dynamic _categories = Enum.GetValues(typeof(Category)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
         private readonly dynamic _statuses = Enum.GetValues(typeof(Status)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
-        //private readonly dynamic _categories = Enum.GetValues(typeof(Category)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
-
 
         public IActionResult Index(string id)
         {
             var temp = service.GetAll();
-
 
             var filters = new Filters(id);
             ViewBag.Filters = filters;
             ViewBag.Categories = _categories;
             ViewBag.Statuses = _statuses;
             ViewBag.DueFilters = Filters.DueFilterValues;
-
-            //IQueryable<ToDo> query = context.ToDoS
-            //    .Include(t => t.Category)
-            //    .Include(t => t.Status);
-
-            //if (filters.HasCategory)
-            //{
-            //    query = query.Where(t => t.CategoryId == filters.CategoryId);
-            //}
-
-            //if (filters.HasStatus)
-            //{
-            //    query = query.Where(t => t.StatusId == filters.StatusId);
-            //}
-
-            //if (filters.HasDue)
-            //{
-            //    var today = DateTime.Today;
-            //    if (filters.IsPast)
-            //    {
-            //        query = query.Where(t => t.DueDate < today);
-            //    }
-            //    else if (filters.IsFuture)
-            //    {
-            //        query = query.Where(t => t.DueDate > today);
-
-            //    }
-            //    else if (filters.IsToday)
-            //    {
-            //        query = query.Where(t => t.DueDate == today);
-            //    }
-            //}
-            //var tasks = query.OrderBy(t => t.DueDate).ToList();
-
-            //return View(tasks);
 
             return View(temp);
         }
