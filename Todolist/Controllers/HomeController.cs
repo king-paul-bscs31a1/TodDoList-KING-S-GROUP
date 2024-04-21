427d55e56cee728abab415ffc33f236cea327dcd
edrel <161002110+king-paul-bscs31a1@users.noreply.github.com>
2024-04-21 17:28:17 +0800

    Update HomeController.cs

--- Todolist/Controllers/HomeController.cs
+++ Todolist/Controllers/HomeController.cs
@@ -15,8 +15,6 @@ namespace Todolist.Controllers
     {
         private readonly dynamic _categories = Enum.GetValues(typeof(Category)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
         private readonly dynamic _statuses = Enum.GetValues(typeof(Status)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
-        //private readonly dynamic _categories = Enum.GetValues(typeof(Category)).OfType<dynamic>().Select(x => new { Key = x, Value = Enum.GetName(x) });
-
 
         public IActionResult Index(string id)
         {
@@ -28,41 +26,6 @@ namespace Todolist.Controllers
             ViewBag.Categories = _categories;
             ViewBag.Statuses = _statuses;
             ViewBag.DueFilters = Filters.DueFilterValues;
-
-            //IQueryable<ToDo> query = context.ToDoS
-            //    .Include(t => t.Category)
-            //    .Include(t => t.Status);
-
-            //if (filters.HasCategory)
-            //{
-            //    query = query.Where(t => t.CategoryId == filters.CategoryId);
-            //}
-
-            //if (filters.HasStatus)
-            //{
-            //    query = query.Where(t => t.StatusId == filters.StatusId);
-            //}
-
-            //if (filters.HasDue)
-            //{
-            //    var today = DateTime.Today;
-            //    if (filters.IsPast)
-            //    {
-            //        query = query.Where(t => t.DueDate < today);
-            //    }
-            //    else if (filters.IsFuture)
-            //    {
-            //        query = query.Where(t => t.DueDate > today);
-
-            //    }
-            //    else if (filters.IsToday)
-            //    {
-            //        query = query.Where(t => t.DueDate == today);
-            //    }
-            //}
-            //var tasks = query.OrderBy(t => t.DueDate).ToList();
-
-            //return View(tasks);
 
             return View(temp);
         }
@@ -92,25 +55,6 @@ namespace Todolist.Controllers
             return View(task);
 
         }
-
-        //[HttpPost]
-        //public IActionResult Filter(string[] filter)
-        //{
-        //    string id = string.Join("-", filter);
-        //    return RedirectToAction("Index", new { ID = id });
-        //}
-        //[HttpPost]
-        //public IActionResult MarkComplete([FromRoute] string id, ToDo selected)
-        //{
-        //    selected = context.ToDoS.Find(selected.Id);
-
-        //    if (selected != null)
-        //    {
-        //        selected.StatusId = "closed";
-        //        context.SaveChanges();
-        //    }
-        //    return RedirectToAction("Index", new { ID = id });
-        //}
 
         [HttpPost]
         public async Task<IActionResult> MarkComplete([FromRoute] int id, ToDo selected)
